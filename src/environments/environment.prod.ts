5d7cabb1d0245eb2970afdbb9ba2e31e5368d0ad
Airbone223 <60809426+Airbone223@users.noreply.github.com>
2021-04-23 17:15:58 +0300

    create guthub repositories

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBPAV1svPRK0thGOE1MyzfOueFZ3tJ3vqA',
+  fbDbUrl: 'https://angular-blog-eb007-default-rtdb.firebaseio.com/'
+};
